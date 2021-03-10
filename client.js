7b16002d0615aaa0e8f9b68ff132443dde5b02e8
Hongjin Zhu <summerellaooo@gmail.com>
2021-03-10 18:45:03 +0000

    update

---
+++ client.js
@@ -0,0 +1,14 @@
+const net = require('net');
+const connet = function() {
+  const conn = net.createConnection({
+    host: "135.23.222.131",
+    port: 50542
+  });
+  conn.setEncoding('utf8');
+  conn.on('data', (data) => {
+    console.log('you ded cuz you idled');
+  })
+  return conn;
+}
+
+module.exports = connet;
--- play.js
+++ play.js
@@ -1,18 +1,9 @@
 // PORT: 50542
 //HOST: "135.23.222.131"
+const connet = require('./client');
 
-const net = require('net');
-const connet = function() {
-  const conn = net.createConnection({
-    host: "135.23.222.131",
-    port: 50542
-  });
-  conn.setEncoding('utf8');
-  conn.on('data', (data) => {
-    console.log('you ded cuz you idled');
-  })
-  return conn;
-}
-
-console.log('Connecting...');
+console.log('connecting ...');
 connet();
+
+
+
