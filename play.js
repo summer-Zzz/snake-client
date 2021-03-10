b175a93e3d6b94b9fc41f8de34f9c3fd3cebb76c
Hongjin Zhu <summerellaooo@gmail.com>
2021-03-10 19:55:29 +0000

    updated snake

--- play.js
+++ play.js
@@ -1,9 +1,25 @@
-// PORT: 50542
-//HOST: "135.23.222.131"
+
 const connect = require('./client');
 
 console.log('connecting ...');
 connect();
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on('data', (key) => {
+    handleUserInput(key)
+  });
+  stdin.resume();
+  return stdin;
+}
 
+const handleUserInput = function(key){
+    if (key === '\u0003') {
+      process.exit();
+  };
+};
+
+setupInput()
 
